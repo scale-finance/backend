08edc49db7375f7cf7215726078aec2120b3d969
Eduardo Lopez <52265974+elopez00@users.noreply.github.com>
2023-02-10 10:55:33 -0500

    added findByEmail to User class (#6)
    In this commit, I added the findByEmail function the the User class for
    the purpose of tying together the login. This way, users can be
    retrieved, including their hashed password just by their email.

--- test/models/User.spec.ts
+++ test/models/User.spec.ts
@@ -3,89 +3,129 @@ import { User } from "../../src/models";
 import { UserType } from "../../src/types/models";
 
 describe("User Model", () => {
-    it("can create user", async () => {
-        const user: UserType = {
-            fullName: "John Doe",
-            email: "jdoe@example.com",
-            password: "password",
-        };
+    describe("create()", () => {
+        it("can create user", async () => {
+            const user: UserType = {
+                fullName: "John Doe",
+                email: "jdoe@example.com",
+                password: "password",
+            };
 
-        prismaMock.user.create.mockResolvedValue({
-            ...user,
-            id: expect.anything(),
-            password: expect.anything(),
+            prismaMock.user.create.mockResolvedValue({
+                ...user,
+                id: expect.anything(),
+                password: expect.anything(),
+            });
+
+            await expect(User.create(user)).resolves.toEqual({
+                email: user.email,
+                fullName: user.fullName,
+                id: expect.anything(),
+                password: expect.anything(),
+            });
         });
 
-        await expect(User.create(user)).resolves.toEqual({
-            email: user.email,
-            fullName: user.fullName,
-            id: expect.anything(),
-        });
-    });
+        it("will not create user that already exists", async () => {
+            const user = {
+                id: expect.anything(),
+                fullName: "John Doe",
+                email: "jdoe@example.com",
+                password: "password",
+            };
 
-    it("will not create user that already exists", async () => {
-        const user = {
-            id: expect.anything(),
-            fullName: "John Doe",
-            email: "jdoe@example.com",
-            password: "password",
-        };
+            prismaMock.user.findFirst.mockResolvedValue(user);
 
-        prismaMock.user.findFirst.mockResolvedValue(user);
-
-        await expect(User.create(user)).rejects.toThrowError(
-            "User already exists."
-        );
-    });
-
-    it("will throw error if user creation fails", async () => {
-        const user: UserType = {
-            fullName: "John Doe",
-            email: "jdoe@example.com",
-            password: "password",
-        };
-
-        prismaMock.user.create.mockRejectedValue(new Error("Failed to create user in database."));
-        prismaMock.user.findFirst.mockResolvedValue(null);
-
-        await expect(User.create(user)).rejects.toThrowError(
-            "Failed to create user in database."
-        );
-    });
-
-    it("can find user by id", async () => {
-        const user = {
-            id: expect.anything(),
-            fullName: "John Doe",
-            email: "jdoe@example.com",
-        };
-
-        prismaMock.user.findUnique.mockResolvedValue({
-            ...user,
-            password: expect.anything(),
+            await expect(User.create(user)).rejects.toThrowError(
+                "User already exists."
+            );
         });
 
-        await expect(User.findById(user.id)).resolves.toEqual(user);
+        it("will throw error if user creation fails", async () => {
+            const user: UserType = {
+                fullName: "John Doe",
+                email: "jdoe@example.com",
+                password: "password",
+            };
+
+            prismaMock.user.create.mockRejectedValue(new Error("Failed to create user in database."));
+            prismaMock.user.findFirst.mockResolvedValue(null);
+
+            await expect(User.create(user)).rejects.toThrowError(
+                "Failed to create user in database."
+            );
+        });
     });
+    
+    describe("findById()", () => {
+        it("can find user by id", async () => {
+            const user = {
+                id: expect.anything(),
+                fullName: "John Doe",
+                email: "jdoe@example.com",
+                password: expect.anything(),
+            };
 
-    it("will return null if user is not found", async () => {
-        prismaMock.user.findUnique.mockResolvedValue(null);
+            prismaMock.user.findUnique.mockResolvedValue(user);
 
-        await expect(User.findById("id")).resolves.toEqual(null);
+            await expect(User.findById(user.id)).resolves.toEqual(user);
+        });
+
+        it("will return null if user is not found", async () => {
+            prismaMock.user.findUnique.mockResolvedValue(null);
+
+            await expect(User.findById("id")).resolves.toEqual(null);
+        });
+
+        it("will throw error if user find fails", async () => {
+            prismaMock.user.findUnique.mockRejectedValue(new Error("Failed to find user in database."));
+
+            await expect(User.findById("id")).rejects.toThrowError(
+                "Failed to find user in database."
+            );
+        });
     });
-
-    it("will hash password", async () => {
-        const testPassword = "something";
+    
+    describe("hash()", () => {
+        it("will hash password", async () => {
+            const testPassword = "something";
         
-        // @ts-ignore
-        await expect(User.hash(testPassword)).resolves.not.toEqual(testPassword);
+            // @ts-ignore
+            await expect(User.hash(testPassword)).resolves.not.toEqual(testPassword);
+        });
     });
 
-    it("will throw error if user find fails", async () => {
-        prismaMock.user.findUnique.mockRejectedValue(new Error("Failed to find user in database."));
+    describe("findByEmail()", () => {
+        it("will find a user by id", () => {
+            const user = {
+               fullName: "John Doe",
+               email: "jdoe@example.com",
+               id: expect.anything(),
+               password: expect.anything(),
+            };
 
-        await expect(User.findById("id")).rejects.toThrowError(
-            "Failed to find user in database."
-        );
+            prismaMock.user.findFirst.mockResolvedValue(user);
+
+            expect(User.findByEmail(user.email)).resolves.toEqual(user);
+        });
+
+        it("will return null if no user is found", () => {
+            const user = {
+                email: "test@email.com",
+            };
+
+            prismaMock.user.findFirst.mockResolvedValue(null);
+
+            expect(User.findByEmail(user.email)).resolves.toEqual(null);
+        });
+
+        it("will throw if there is an internal server error", () => {
+            const user = {
+                email: "test@email.com",
+            };
+
+            prismaMock.user.findFirst.mockRejectedValue(new Error("something happened"));
+
+            expect(User.findByEmail(user.email)).rejects.toThrowError("Failed to retrieve user from database");
+        });
     });
 });
